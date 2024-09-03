962eb0f9c27a7caa16e8daa587cadd1452e49efb
Nicolás Asenjo <78203606+Miraidesu@users.noreply.github.com>
2024-09-03 13:04:08 -0400

    style added

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -14,22 +14,20 @@ export default function Info() {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Preguntas frecuentes</h1>
-      <div>
-	  	<Accordion className="border-2 rounded-md p-2" type="single" collapsible>
-			<AccordionItem  value="item-1">
+	  	<Accordion type="single" collapsible>
+			<AccordionItem className="m-2 border-2 rounded-md px-6"  value="item-1">
 				<AccordionTrigger  className="font-bold">Pregunta</AccordionTrigger>
 				<AccordionContent>
 					Wena choro
 				</AccordionContent>
 			</AccordionItem>
-			<AccordionItem value="item-2">
+			<AccordionItem className="m-2 border-2 rounded-md px-6"  value="item-2">
 				<AccordionTrigger className="font-bold">Pregunta</AccordionTrigger>
 				<AccordionContent>
 					ola mundo
 				</AccordionContent>
 			</AccordionItem>
 		</Accordion>
-      </div>
     </div>
   )
 }
