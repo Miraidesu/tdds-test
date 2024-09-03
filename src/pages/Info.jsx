ee7070a4899ee2476a6ce81f9516f3af8ce3bb66
Nicolás Asenjo <78203606+Miraidesu@users.noreply.github.com>
2024-09-03 12:56:46 -0400

    info style updated

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -4,25 +4,31 @@ import { Card,
 	CardHeader, 
 	CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
+import { Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger
+ } from "@/components/ui/accordion"
 
 export default function Info() {
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-8 text-center">Información</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-		<Card className="overflow-hidden">
-		<img src="https://via.placeholder.com/500"
-			className="w-full h-48 object-cover"/>
-		<CardHeader>
-			<CardTitle>Título</CardTitle>
-		</CardHeader>
-		<CardContent>
-			<p className="text-muted-foreground">sd</p>
-		</CardContent>
-		<CardFooter className="justify-center">
-			<Button className="w-1/2">Leer</Button>
-		</CardFooter>
-		</Card>
+      <h1 className="text-3xl font-bold mb-8 text-center">Preguntas frecuentes</h1>
+      <div>
+	  	<Accordion className="border-2 rounded-md p-2" type="single" collapsible>
+			<AccordionItem  value="item-1">
+				<AccordionTrigger  className="font-bold">Pregunta</AccordionTrigger>
+				<AccordionContent>
+					Wena choro
+				</AccordionContent>
+			</AccordionItem>
+			<AccordionItem value="item-2">
+				<AccordionTrigger className="font-bold">Pregunta</AccordionTrigger>
+				<AccordionContent>
+					ola mundo
+				</AccordionContent>
+			</AccordionItem>
+		</Accordion>
       </div>
     </div>
   )
