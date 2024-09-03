b120b7dfd286af45857fea0eb5b0b3f8c4d4603b
Nicolás Asenjo <78203606+Miraidesu@users.noreply.github.com>
2024-09-03 12:56:51 -0400

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Appointments from "./pages/Appointments"
-import CreateProfile from "./pages/createProfile"
+import CreateProfile from "./pages/CreateProfile"
 import Dashboard from "./pages/Dashboard"
 import Info from "./pages/Info"
 import Login from "./pages/Login"
@@ -10,7 +10,7 @@ import UserScheduling from "./pages/UserScheduling"
 function App() {
   return (
     <div>
-      <Appointments />
+      <Info />
     </div>
   ) 
 }
