f98ee1c9577d8af136c71946543b69ca24c2d8af
Miraidesu <78203606+Miraidesu@users.noreply.github.com>
2024-09-02 22:26:54 -0400

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import CreateProfile from "./pages/createProfile"
 import Info from "./pages/Info"
+import Login from "./pages/Login"
 import Register from "./pages/Register"
 import UserScheduling from "./pages/UserScheduling"
 
 function App() {
   return (
     <div>
-      <Register />
+      <Info />
     </div>
   ) 
 }
