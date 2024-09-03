a828a78cf3ba529bfc4f0ec7dee19ae949b2ce6d
Miraidesu <78203606+Miraidesu@users.noreply.github.com>
2024-09-02 22:27:00 -0400

    init register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,57 +1,96 @@
+import { useState } from "react"
+import { es } from "date-fns/locale"
 import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+import { Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle } from "@/components/ui/card"
+import { Select, 
+  SelectContent, 
+  SelectItem, 
+  SelectTrigger, 
+  SelectValue } from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
+import { Calendar } from "@/components/ui/calendar"
 import { Label } from "@/components/ui/label"
 
-export default function Register() {
+export default function UserScheduling() {
+  const [date, setDate] = useState(Date())
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Registro</Button>
-      </DialogTrigger>
-      <DialogContent className="rounded-md w-96">
-        <DialogHeader className="space-y-4">
-          <DialogTitle>Registro</DialogTitle>
-          <DialogDescription>
-            Ingrese sus datos para crear una cuenta
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-6 items-center gap-4">
-            <Label htmlFor="rut" className="text-right col-start-2">
-              RUT
-            </Label>
-            <Input
-              id="rut"
-              placeholder="12345678"
-              className="col-span-2"/>
-            <Input
-              id="dig"
-              placeholder="K"/>
-          </div>
-          <div className="grid grid-cols-6 items-center gap-4"> 
-            <Label htmlFor="password" className="text-right col-span-2">
-              Contraseña
-            </Label>
-            <Input
-              id="password"
-              type="password"
-              className="col-start-3 col-span-3"/>
-          </div>
-        </div>
-        <DialogFooter className="grid grid-cols-6 gap-4">
-          <Button type="submit" className="col-start-2 col-span-4">Entrar</Button>
-          <Button type="submit" variant="outline" className="col-start-2 col-span-4">Registrar con Google</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <div className="bg-slate-950 max-h-svh">
+      <Card className="w-[325px] max-w-md mx-auto">
+        <CardHeader>
+          <CardTitle>Registro</CardTitle>
+          <CardDescription>Ingrese sus datos para crear una cuenta</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form >
+            <div className="space-y-4">
+              <Label htmlFor="rut">
+                RUT
+              </Label>
+              <div className="grid grid-cols-5 gap-4">
+                <Input
+                  id="rut"
+                  placeholder="12345678"
+                  className="col-span-2"/>
+                <Input
+                  id="dig"
+                  placeholder="K"/>
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="name">Nombre(s)</Label>
+                <Input placeholder="Nombre"/>
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="lastname">Apellido(s)</Label>
+                <Input placeholder="Apellido"/>
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="birthday">Fecha de nacimiento</Label>
+                <div className="grid grid-cols-10 gap-4">
+                  <Input className="col-span-2" placeholder="31"/>
+                  <Select >
+                    <SelectTrigger className="col-span-5" id="month">
+                      <SelectValue placeholder="Enero" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="enero">Enero</SelectItem>
+                      <SelectItem value="febrero">Febrero</SelectItem>
+                      <SelectItem value="marzo">Marzo</SelectItem>
+                      <SelectItem value="abril">Abril</SelectItem>
+                      <SelectItem value="mayo">Mayo</SelectItem>
+                      <SelectItem value="junio">Junio</SelectItem>
+                      <SelectItem value="julio">Julio</SelectItem>
+                      <SelectItem value="agosto">Agosto</SelectItem>
+                      <SelectItem value="septiembre">Septiembre</SelectItem>
+                      <SelectItem value="octubre">Octubre</SelectItem>
+                      <SelectItem value="noviembre">Noviembre</SelectItem>
+                      <SelectItem value="diciembre">Diciembre</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <Input className="col-span-3" placeholder="2024"/>
+                </div>
+              </div>
+
+              <div className="space-y-2">
+              <Label htmlFor="email">Correo electronico</Label>
+              <Input placeholder="ejemplo@mail.cl"/>
+              </div>
+
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Registrarse</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
