79c9e158ae5ae02b4b1fc1045e2a7ea6ef649491
Nicolás Asenjo <78203606+Miraidesu@users.noreply.github.com>
2024-10-15 12:30:12 -0300

    componente navbar creado

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,31 +1,58 @@
 import {
-	Menubar,
-	MenubarContent,
-	MenubarItem,
-	MenubarMenu,
-	MenubarSeparator,
-	MenubarShortcut,
-	MenubarTrigger,
-  } from "@/components/ui/menubar"
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu"
+import Login from "./Login";
+import { Link } from 'react-router-dom';
+import { bg } from "date-fns/locale";
 
 export default function NavBar() {
 	return (
-	<header>
-		<Menubar>
-  <MenubarMenu>
-    <MenubarTrigger>File</MenubarTrigger>
-    <MenubarContent>
-      <MenubarItem>
-        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-      </MenubarItem>
-      <MenubarItem>New Window</MenubarItem>
-      <MenubarSeparator />
-      <MenubarItem>Share</MenubarItem>
-      <MenubarSeparator />
-      <MenubarItem>Print</MenubarItem>
-    </MenubarContent>
-  </MenubarMenu>
-</Menubar>
+	<header className="flex justify-center">
+    <NavigationMenu>
+      <NavigationMenuList>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <Link to="/">Inicio</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <Link to="/">Noticias</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <Link to="/">Nosotros</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem >
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <Link to="/">Pide tu hora</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Cuenta</NavigationMenuTrigger>
+          <NavigationMenuContent className="grid gap-3 p-4">
+            <Login/>
+            <Link to="/register">Registrarse</Link>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      
+      </NavigationMenuList> 
+    </NavigationMenu>
 	</header>
 	)
 }
