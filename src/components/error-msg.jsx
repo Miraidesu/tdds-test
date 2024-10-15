42d9541e3b457a9c039488ef914a5268115acb2b
Miraidesu <78203606+Miraidesu@users.noreply.github.com>
2024-10-15 00:55:28 -0300

    fix

--- src/components/error-msg.jsx
+++ src/components/error-msg.jsx
@@ -1,7 +1,7 @@
-export default function ErrorMsg({text}) {
+export default function ErrorMsg({children}) {
   return (
 	<p className="text-xs font-semibold text-red-500">
-		{text}
+		{children}
 	</p>
 	)
 }
