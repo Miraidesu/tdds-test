e774eb19007cf5775d967c3f2623111094dd2819
Miraidesu <78203606+Miraidesu@users.noreply.github.com>
2024-11-12 02:02:05 -0300

    animacion carga

--- src/pages/UserScheduling.jsx
+++ src/pages/UserScheduling.jsx
@@ -16,9 +16,16 @@ import { Select,
 import { Calendar } from "@/components/ui/calendar"
 import { Label } from "@/components/ui/label"
 import { useNavigate } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
+import { set } from "date-fns";
 
 export default function UserScheduling() {
   const [userValid, setUserValid] = useState(false)
+  // Verificacion en carga de elementos
+  const [servLoading, setServLoading] = useState(true)
+  const [docLoading, setDocLoading] = useState(true)
+  const [timeLoading, setTimeLoading] = useState(true)
+  // Guardado de datos
   const [service, setService]   = useState("")
   const [date, setDate]         = useState(null)
   const [doctor, setDoctor]     = useState("")
@@ -94,6 +101,7 @@ useEffect(() => {
         const response = await fetch("http://localhost:5000/api/userSchedule/servicios");
         const data = await response.json();
         setServiceList(data);
+        setServLoading(false)
       } catch (error) {
         console.error("Error al obtener servicios:", error);
       }
@@ -107,6 +115,7 @@ useEffect(() => {
         const response = await fetch("http://localhost:5000/api/userSchedule/medicos");
         const data = await response.json();
         setDoctorList(data);
+        setDocLoading(false)
         console.log(data)
       } catch (error) {
         console.error("Error al obtener médicos:", error);
@@ -121,6 +130,7 @@ useEffect(() => {
         const response = await fetch(`http://localhost:5000/api/userSchedule/horarios/${doctor}`);
         const data = await response.json();
         setTimeSlotList(data);
+        setTimeLoading(false)
         console.log(data)
       } catch (error) {
         console.error("Error al obtener horarios:", error);
@@ -153,7 +163,7 @@ useEffect(() => {
     "14:00",
     "15:00"
   ]
-
+  
   if (userValid) {
     return (
     <div className="bg-slate-950 max-h-svh">
@@ -166,19 +176,21 @@ useEffect(() => {
           <form onSubmit={handleSubmit}>
             <div className="space-y-4">
 
-              <div className="space-y-2">
-                <Label htmlFor="servicio">¿Qué servicio necesita?</Label>
-                <Select onValueChange={handleService} required>
-                  <SelectTrigger id="servicio">
-                    <SelectValue placeholder="Seleccionar servicio" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {serviceList.map((i) => 
-                      <SelectItem value={i.label}>{i.label}</SelectItem>
-                    )}
-                  </SelectContent>
-                </Select>
-              </div>
+              {servLoading ? <Skeleton className="w-[275px] h-[72px]"/> : (
+                <div className="space-y-2">
+                  <Label htmlFor="servicio">¿Qué servicio necesita?</Label>
+                  <Select onValueChange={handleService} required>
+                    <SelectTrigger id="servicio">
+                      <SelectValue placeholder="Seleccionar servicio" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {serviceList.map((i) => 
+                        <SelectItem value={i.label}>{i.label}</SelectItem>
+                      )}
+                    </SelectContent>
+                  </Select>
+                </div>
+              )}
 
               { service ?
                 <div className="space-y-2">
@@ -192,21 +204,27 @@ useEffect(() => {
                 </div> 
               : null }
 
-              { date ?
-              <div className="space-y-2">
-                <Label htmlFor="medico">Seleccionar médico</Label>
-                <Select onValueChange={handleDoctor} required>
-                  <SelectTrigger id="medico">
-                    <SelectValue placeholder="Seleccionar medico" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {doctorList.map((i) => 
-                      <SelectItem value={i.label}>{i.label}</SelectItem>
-                    )}
-                  </SelectContent>
-                </Select>
-              </div>
-              : null }
+              {date ? (
+                docLoading ? (
+                  <Skeleton className="w-[275px] h-[72px]" />
+                ) : (
+                  <div className="space-y-2">
+                    <Label htmlFor="medico">Seleccionar médico</Label>
+                    <Select onValueChange={handleDoctor} required>
+                      <SelectTrigger id="medico">
+                        <SelectValue placeholder="Seleccionar medico" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {doctorList.map((i) => (
+                          <SelectItem key={i.label} value={i.label}>
+                            {i.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                )
+              ) : null}
 
               { doctor ?
               <div className="space-y-2">
@@ -216,8 +234,8 @@ useEffect(() => {
                     <SelectValue placeholder="Seleccionar hora" />
                   </SelectTrigger>
                   <SelectContent>
-                    {timeSlotList.map((i) => 
-                      <SelectItem value={i.value}>{i.label}</SelectItem>
+                    {timeSlots.map((i) => 
+                      <SelectItem value={i}>{i}</SelectItem>
                     )}
                   </SelectContent>
                 </Select>
