aefed250eff6bfdc611853075a6d023b251a9190
Miraidesu <78203606+Miraidesu@users.noreply.github.com>
2024-11-11 22:10:29 -0300

    manejo sesiones

--- src/backend/app.py
+++ src/backend/app.py
@@ -305,14 +305,19 @@
 import os
 from xml.sax import parseString
 import bcrypt
-from flask import Flask, request, jsonify
+from datetime import timedelta
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
+from flask_jwt_extended import (
+	JWTManager, 
+    get_jwt_identity,
+	create_access_token, 
+	jwt_required )
 
 load_dotenv()
-
 
 def calcular_dv(rut):
     suma = 0
@@ -331,15 +336,16 @@ def calcular_dv(rut):
     else:
         return str(resto)
 
-
-
 # Load environment variables
 DB_URL = os.getenv("DATABASE_URL")  # e.g., "dbname-orgname.turso.io"
 AUTH_TOKEN = os.getenv("DATABASE_TOKEN")  # Your JWT token for authentication
-
+FLASK_SECRET_KEY = os.getenv("SECRET_KEY")
 # Create Flask app
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+app.config["SECRET_KEY"] = FLASK_SECRET_KEY
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173", "supports_credentials": True}})
+JWTManager(app)
 
 # Set up SQLAlchemy engine
 engine = create_engine(f"{DB_URL}?authToken={AUTH_TOKEN}")
@@ -421,41 +427,67 @@ def register_user():
 def login():
     data = request.json
     rut_num = data.get("rutNum")
-    rut_dig = data.get("rutDig")
     password = data.get("password")
 
     # Calcular el dígito verificador esperado
-    dig = calcular_dv(rut_num)
+    # dig = calcular_dv(rut_num)
 
     # Normalizar ambos dígitos verificadores a mayúsculas y como strings para evitar errores
-    if str(dig).upper() == str(rut_dig).upper():
         
         # Convertir RUT a número si es necesario
-        try:
-            rut_num = int(rut_num)
-        except ValueError:
-            return jsonify({'message': "El RUT debe ser un número"}), 400
+    try:
+        rut_num = int(rut_num)
+    except ValueError:
+        return jsonify({'message': "El RUT debe ser un número"}), 400
 
-        # Consultar en la base de datos para obtener el hash de la contraseña del usuario
-        with engine.connect() as conn:
-            query = text("SELECT password FROM Usuario WHERE Rut = :rut")
+    # Consultar en la base de datos para obtener el hash de la contraseña del usuario
+    with engine.connect() as conn:
+        query = text("SELECT password FROM Usuario WHERE Rut = :rut")
+        result = conn.execute(query, {"rut": rut_num}).fetchone()
+
+        # Verificar si el usuario existe
+        if result is None:
+            return jsonify({'message': "Datos incorrectos"}), 401
+
+        # Validar la contraseña usando bcrypt
+        stored_password_hash = result[0]
+        if bcrypt.checkpw(password.encode('utf-8'), stored_password_hash.encode('utf-8')):
+            user_type = None
+            query = text("SELECT cod_tipo_user FROM Usuario WHERE Rut = :rut")
             result = conn.execute(query, {"rut": rut_num}).fetchone()
 
-            # Verificar si el usuario existe
-            if result is None:
-                return jsonify({'message': "Datos incorrectos"}), 401
+            if result is not None:
+                user_type = result[0]
 
-            # Validar la contraseña usando bcrypt
-            stored_password_hash = result[0]
-            if bcrypt.checkpw(password.encode('utf-8'), stored_password_hash.encode('utf-8')):
-                return jsonify({'message': "Login exitoso"}), 200
-            else:
-                return jsonify({'message': "Datos incorrectos"}), 401
-    else:
-        print(f"RUT ingresado: {data['rutNum']}, DV calculado: {dig}, DV ingresado: {data['rutDig']}")
-        return jsonify({'message': "Datos incorrectos"}), 401
+            access_token = create_access_token(identity={
+                "user": rut_num,
+                "user_type": user_type
+            })
 
+            response = make_response(jsonify({
+                'message': "Login exitoso",
+                "data": {
+                    "user": rut_num,
+                    "user_type": user_type
+                }}))
+            response.set_cookie(
+                key="access_token_cookie", value=access_token,
+                httponly=True,
+                samesite='Strict',
+                secure=False
+            )
+
+            return response, 200
+        else:
+            return jsonify({'message': "Datos incorrectos"}), 401
         
+    return jsonify({'message': "Datos incorrectos"}), 401
+
+@app.route("/get_credentials", methods=["GET"])
+@jwt_required(locations=["cookies"]) 
+def get_credentials():
+    current_user = get_jwt_identity()
+    return jsonify(current_user), 200
 
 # Ruta para registrar una reserva
 @app.route('/api/userSchedule', methods=['POST'])
