46d80c7f0dce006fc969a8ad213a4930ae9d0920
Miraidesu <78203606+Miraidesu@users.noreply.github.com>
2024-11-12 01:38:27 -0300

    chequeo sesion navbar

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -10,12 +10,57 @@ import {
   navigationMenuTriggerStyle
 } from "@/components/ui/navigation-menu"
 import Login from "./Login";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { bg } from "date-fns/locale";
+import { useEffect, useState } from "react";
 
 export default function NavBar() {
+  const [userLogged, setUserLogged] = useState(false)
+  const navigate = useNavigate();
+
+  const isUserLogged = async () => {
+    const response = await fetch("http://localhost:5000/get_credentials", {
+      method: "GET", // Tipo de solicitud
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Esto asegura que las cookies se envíen con la solicitud
+    });
+  
+    if (response.ok) {
+      const data = await response.json();
+      if (data.user) {
+        setUserLogged(true)
+      }
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/logout", {
+        method: "POST",
+        credentials: "include"
+      })
+      
+      if (response.ok) {
+        navigate("/")
+      } else {
+        const result = await response.json()
+        alert(result.message)
+      }
+    } catch (error) {
+      alert("Error de conexión en el servidor:", error);
+    }
+  }
+
+  useEffect((
+    ) => {
+      isUserLogged();
+    }
+  , []);
+
 	return (
-    <div className="bg-white fixed top-0 left-0 w-full z-10 shadow-md">
+  <div className="fixed z-10 top-0 left-0 w-full p-2 bg-white shadow-md">
 	<header className="relative flex justify-center ">
     <NavigationMenu>
       <NavigationMenuList>
@@ -44,16 +89,26 @@ export default function NavBar() {
           </NavigationMenuLink>
         </NavigationMenuItem>
 
-        <NavigationMenuItem >
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            <Link to="/register">Registrarse</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+        {!userLogged ? (
+          <div className="flex">
+          <NavigationMenuItem >
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/register">Registrarse</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-        <NavigationMenuItem >
-          <Login />
-        </NavigationMenuItem>
-      
+          <NavigationMenuItem >
+            <Login />
+          </NavigationMenuItem>
+        </div>
+        ) : (
+          <NavigationMenuItem >
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/logout" onClick={handleLogout}>Cerrar sesión</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        )} 
+
       </NavigationMenuList> 
     </NavigationMenu>
 	</header>
