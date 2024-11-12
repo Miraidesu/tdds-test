09b6fe390cccf23bcf11a702c8124de2043a40f7
Nicolás Asenjo <78203606+Miraidesu@users.noreply.github.com>
2024-11-12 12:08:24 -0300

    date listo

--- src/pages/UserScheduling.jsx
+++ src/pages/UserScheduling.jsx
@@ -17,7 +17,7 @@ import { Calendar } from "@/components/ui/calendar"
 import { Label } from "@/components/ui/label"
 import { useNavigate } from "react-router-dom";
 import { Skeleton } from "@/components/ui/skeleton";
-import { set } from "date-fns";
+import { format, parse } from "date-fns";
 
 export default function UserScheduling() {
   const [userValid, setUserValid] = useState(false)
@@ -40,8 +40,6 @@ export default function UserScheduling() {
   });
 
   const navigate = useNavigate();
-
-  
 
 useEffect(() => {
     const getUserType = async () => {
@@ -73,6 +71,8 @@ useEffect(() => {
       doctor,
       timeSlot,
     };
+
+    console.log(data)
   
     try {
       const response = await fetch("http://localhost:5000/api/userSchedule", {
@@ -112,6 +112,7 @@ useEffect(() => {
   useEffect(() => {
     const fetchMedicos = async () => {
       try {
+        setDocLoading(true)
         const response = await fetch("http://localhost:5000/api/userSchedule/medicos");
         const data = await response.json();
         setDoctorList(data);
@@ -129,9 +130,33 @@ useEffect(() => {
       try {
         const response = await fetch(`http://localhost:5000/api/userSchedule/horarios/${doctor}`);
         const data = await response.json();
-        setTimeSlotList(data);
+           
+        const timeSlots = [
+          {hora: "08:00", state: true},
+          {hora: "09:00", state: true},
+          {hora: "10:00", state: true},
+          {hora: "11:00", state: true},
+          {hora: "12:00", state: true},
+          {hora: "13:00", state: true},
+          {hora: "14:00", state: true},
+          {hora: "15:00", state: true},
+        ]
+        
+        const dates = data.map(i => {
+          const d = new Date(i)
+          return format(d, "HH:mm")
+        })
+
+        const updatedTimeSlots = timeSlots.map(slot => {
+          if (dates.includes(slot.hora)) {
+            return { ...slot, state: false };
+          }
+          return slot;
+        });
+
+        setTimeSlotList(updatedTimeSlots);
         setTimeLoading(false)
-        console.log(data)
+        console.log(updatedTimeSlots)
       } catch (error) {
         console.error("Error al obtener horarios:", error);
       }
@@ -153,16 +178,10 @@ useEffect(() => {
     }
   };
 
-  const timeSlots = [
-    "08:00",
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00"
-  ]
+  const handleDate = (date) => {
+    setDate(format(date, "yyyy-MM-dd"));
+    console.log(format(date, "yyyy-MM-dd"))
+  } 
   
   if (userValid) {
     return (
@@ -198,8 +217,8 @@ useEffect(() => {
                   <Calendar className="rounded-md border"
                     locale={es}
                     mode="single"
-                    selected={date}
-                    onSelect={setDate}
+                    selected={date ? parse(date, "yyyy-MM-dd", new Date()) : date}
+                    onSelect={handleDate}
                     disabled={(date) => date < new Date()} />
                 </div> 
               : null }
@@ -225,8 +244,11 @@ useEffect(() => {
                   </div>
                 )
               ) : null}
-
+            
               { doctor ?
+              timeLoading ? (
+                <Skeleton className="w-[275px] h-[72px]" />
+              ) : (
               <div className="space-y-2">
                 <Label htmlFor="hora">¿Qué horario necesita?</Label>
                 <Select onValueChange={(v) => setTimeSlot(v)} required>
@@ -234,13 +256,15 @@ useEffect(() => {
                     <SelectValue placeholder="Seleccionar hora" />
                   </SelectTrigger>
                   <SelectContent>
-                    {timeSlots.map((i) => 
-                      <SelectItem value={i}>{i}</SelectItem>
-                    )}
+                    {timeSlotList.map((i) => (
+                      <SelectItem value={i.hora} disabled={!i.state}>
+                        {i.hora}
+                      </SelectItem>
+                    ))}
                   </SelectContent>
                 </Select>
               </div>
-              : null }
+              ) : null }
             </div>
           </form>
         </CardContent>
